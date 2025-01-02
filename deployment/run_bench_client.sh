2884b8e2a1c837824ec3816c0d015fa5e2635078
veit <veit.laule@gmail.com>
2025-01-03 00:06:22 +0100

    Fix tar command

--- deployment/run_bench_client.sh
+++ deployment/run_bench_client.sh
@@ -2,7 +2,7 @@ cd /pgvector_benchmark
 
 sudo gsutil cp gs://bench-data-bucket/trace.tar.gz /pgvector_benchmark/benchmark
 
-sudo tar -xvf /pgvector_benchmark/benchmark/trace.tar.gz /pgvector_benchmark/benchmark/trace
+sudo tar -xvf /pgvector_benchmark/benchmark/trace.tar.gz -C /pgvector_benchmark/benchmark/trace
 
 python3 -m pip install -r requirements.txt
 
