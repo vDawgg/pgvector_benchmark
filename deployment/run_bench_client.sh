abe9ac305cb5518a575e3eeba68b24295d0413a4
veit <veit.laule@gmail.com>
2025-01-03 19:01:18 +0100

    adjust paths once more

--- deployment/run_bench_client.sh
+++ deployment/run_bench_client.sh
@@ -1,9 +1,6 @@
-cd /pgvector_benchmark
-
-gsutil cp gs://bench-data-bucket/trace.tar.gz ~/pgvector_benchmark/benchmark
-
-tar -xvf /pgvector_benchmark/benchmark/trace.tar.gz -C /pgvector_benchmark/benchmark
-
+cd /
+gsutil cp gs://bench-data-bucket/trace.tar.gz ./pgvector_benchmark/benchmark
+tar -xvf ./pgvector_benchmark/benchmark/trace.tar.gz -C ./pgvector_benchmark/benchmark
+cd ./pgvector_benchmark
 python3 -m pip install -r requirements.txt
-
 python3 main.py $1
