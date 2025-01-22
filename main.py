739b7be358350aca91ce214b2e9783a7aaa60f7c
veit <veit.laule@gmail.com>
2025-01-22 21:16:58 +0100

    fix typing

--- main.py
+++ main.py
@@ -32,6 +32,6 @@ if __name__ == '__main__':
     db.teardown()
 
     async_db = AsyncDB(pg_url)
-    asyncio.run(execute_benchmark(async_db, indexing_method, run_number, request_per_second))
+    asyncio.run(execute_benchmark(async_db, indexing_method, run_number, int(request_per_second)))
 
     print('DONE')
