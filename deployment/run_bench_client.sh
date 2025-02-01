83ec3613a96b3bd55df2949bfa2776dca86b7203
veit <veit.laule@gmail.com>
2025-02-01 23:44:40 +0100

    temporarily remove tmux

--- deployment/run_bench_client.sh
+++ deployment/run_bench_client.sh
@@ -1,8 +1,6 @@
-tmux new -d -s benchmark "bash -c '
-    cd /
-    gsutil cp gs://bench-data-bucket-2/trace.tar.gz ./pgvector_benchmark/benchmark
-    tar -xvf ./pgvector_benchmark/benchmark/trace.tar.gz -C ./pgvector_benchmark/benchmark
-    cd ./pgvector_benchmark
-    python3 -m pip install -r requirements.txt
-    python3 main.py --db_host \"$1\" --run_number \"$2\" --requests_per_second \"$3\" ${4:+--indexing_method \"$4\"}
-'"
+cd /
+gsutil cp gs://bench-data-bucket-2/trace.tar.gz ./pgvector_benchmark/benchmark
+tar -xvf ./pgvector_benchmark/benchmark/trace.tar.gz -C ./pgvector_benchmark/benchmark
+cd ./pgvector_benchmark
+python3 -m pip install -r requirements.txt
+python3 main.py --db_host \"$1\" --run_number \"$2\" --requests_per_second \"$3\" ${4:+--indexing_method \"$4\"}
