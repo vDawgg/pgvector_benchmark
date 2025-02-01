b4877de14eb45e175177b3ece304cfc3a8318089
veit <veit.laule@gmail.com>
2025-02-01 15:18:01 +0100

    Change bucket descriptor

--- deployment/run_bench_client.sh
+++ deployment/run_bench_client.sh
@@ -1,6 +1,6 @@
 tmux new -d -s benchmark "bash -c '
     cd /
-    gsutil cp gs://bench-data-bucket/trace.tar.gz ./pgvector_benchmark/benchmark
+    gsutil cp gs://bench-data-bucket-2/trace.tar.gz ./pgvector_benchmark/benchmark
     tar -xvf ./pgvector_benchmark/benchmark/trace.tar.gz -C ./pgvector_benchmark/benchmark
     cd ./pgvector_benchmark
     python3 -m pip install -r requirements.txt
